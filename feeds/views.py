14744f23b4eb57f579bba4ade58926004b48bb2f
arpansahu <asahu@gammastack.com>
2021-04-12 22:27:03 +0530

    added login required in feeds

--- feeds/views.py
+++ feeds/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 
